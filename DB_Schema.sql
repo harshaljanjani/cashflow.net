abb51cf2822a62597f3dec6f8ada017d957e2233
Harshal Janjani <harshaljanjani@gmail.com>
2023-03-28 22:06:41 +0530

    commit#1->added-db-schema_(Postgres)

---
+++ DB_Schema.sql
@@ -0,0 +1,39 @@
+/*PostgreSQL Schema Of The Database*/
+CREATE TABLE "accounts" (
+  "id" bigserial PRIMARY KEY,
+  "owner" "varchar(not null)",
+  "balance" "bigint(not null)",
+  "currency" "varchar(not null)",
+  "created_at" timestamptz NOT NULL DEFAULT (now())
+);
+
+CREATE TABLE "entries" (
+  "id" bigserial PRIMARY KEY,
+  "account_id" bigint,
+  "amount" "bigint(not null, note: 'it can be negative or positive')",
+  "created_at" timestamptz NOT NULL DEFAULT (now())
+);
+
+CREATE TABLE "transfers" (
+  "id" bigserial PRIMARY KEY,
+  "from_account_id" bigint,
+  "to_account_id" bigint,
+  "amount" "bigint(not null, note: 'it must be positive')",
+  "created_at" timestamptz NOT NULL DEFAULT (now())
+);
+
+CREATE INDEX ON "accounts" ("owner");
+
+CREATE INDEX ON "entries" ("account_id");
+
+CREATE INDEX ON "transfers" ("from_account_id");
+
+CREATE INDEX ON "transfers" ("to_account_id");
+
+CREATE INDEX ON "transfers" ("from_account_id", "to_account_id");
+
+ALTER TABLE "entries" ADD FOREIGN KEY ("account_id") REFERENCES "accounts" ("id");
+
+ALTER TABLE "transfers" ADD FOREIGN KEY ("from_account_id") REFERENCES "accounts" ("id");
+
+ALTER TABLE "transfers" ADD FOREIGN KEY ("to_account_id") REFERENCES "accounts" ("id");
