62ff72a1603a71c85629a64a32a018a85047a837
Harshal Janjani <harshaljanjani@gmail.com>
2023-06-12 09:22:37 +0530

    mod: modified up and down migration, added cmdsref

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 
 Emulating A Bank Backend Framework Using Golang, PostgreSQL and Docker
 
-## PostgreSQL DB Schema Using https://dbdiagram.io
+## PostgreSQL BankDB Schema
 
 <img width="960" alt="image" src="https://user-images.githubusercontent.com/75426551/228307357-193897e3-8053-4b11-a67d-e126cd469b66.png">
---
+++ db/migration/000001_init_schema.up.sql
@@ -0,0 +1,46 @@
+CREATE TABLE "accounts" (
+  "id" bigserial PRIMARY KEY,
+  "owner" varchar NOT NULL,
+  "balance" bigint NOT NULL,
+  "currency" varchar NOT NULL,
+  "created_at" timestamptz NOT NULL DEFAULT (now())
+);
+CREATE TABLE "entries" (
+  "id" bigserial PRIMARY KEY,
+  "account_id" bigint NOT NULL,
+  "amount" bigint NOT NULL, /*Can be negative or positive*/
+  "created_at" timestamptz NOT NULL DEFAULT (now())
+);
+CREATE TABLE "transfers" (
+    "id" bigserial PRIMARY KEY,
+    "from_account_id" bigint NOT NULL,
+    "to_account_id" bigint NOT NULL,
+    "amount" bigint NOT NULL, /*Must be positive*/
+    "created_at" timestamptz NOT NULL DEFAULT (now())
+);
+
+CREATE INDEX ON "accounts" ("owner");
+
+CREATE INDEX ON "entries" ("account_id");
+
+CREATE INDEX ON "transfers" ("from_account_id");
+
+CREATE INDEX ON "transfers" ("to_account_id");
+
+CREATE INDEX ON "transfers" ("from_account_id", "to_account_id");
+
+ALTER TABLE "entries" ADD FOREIGN KEY ("account_id") REFERENCES "accounts" ("id");
+
+ALTER TABLE "transfers" ADD FOREIGN KEY ("from_account_id") REFERENCES "accounts" ("id");
+
+ALTER TABLE "transfers" ADD FOREIGN KEY ("to_account_id") REFERENCES "accounts" ("id");
+
+COMMENT ON COLUMN entries.amount is 'Can be negative or positive';
+
+COMMENT ON COLUMN transfers.amount is 'Must be positive';
+
+-- DROP TABLE "public"."accounts";
+
+-- DROP TABLE "public"."entries";
+
+-- DROP TABLE "public"."transfers";
