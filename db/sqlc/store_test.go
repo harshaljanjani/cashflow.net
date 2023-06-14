580e63a6630b6cc398d9d0ffd54ea1a6094e84f1
Harshal Janjani <harshaljanjani@gmail.com>
2023-06-14 23:17:57 +0530

    deadlock avoidance: possible scenario (TODO: fix)

--- db/sqlc/store_test.go
+++ db/sqlc/store_test.go
@@ -118,3 +118,57 @@ func TestTransferTx(t *testing.T) {
 		require.Equal(t, account1.Balance - int64(n) * amount, updatedAccount1.Balance)
 		require.Equal(t, account2.Balance + int64(n) * amount, updatedAccount2.Balance)
 	}
+
+	// deadlock avoidance: found a possible deadlock scenario -> half the transactions going from account1 to account2, and the other half going the other way around
+	// expected: no change in balance
+	// outcome: deadlock
+	func TestTransferTxDeadlock(t *testing.T) {
+		store := NewStore(testDB)
+	
+		account1 := createRandomAccount(t)
+		account2 := createRandomAccount(t)
+		// concurrency control: best practice to run concurrent co-routines
+		// writing logs (before)
+		fmt.Println(">> before:", account1.Balance, account2.Balance)
+	
+		n := 10 // number of concurrent transactions
+		amount := int64(10) // amount of transfer
+		errs := make(chan error) // connect co-routines (without explicit locking)  
+		for i := 0; i < n/2; i++{
+			fromAccountID := account1.ID
+			toAccountID := account2.ID
+			// idea is to implement half the transactions from account1 to account2, and the other half from account2 to account1
+			if i % 2 == 1{
+				fromAccountID = account2.ID
+				toAccountID = account1.ID
+			}
+			// start new go routine
+			go func(){
+				// removed adding value to the context
+				ctx := context.Background()
+				_ , err := store.TransferTx(ctx, TransferTxParams{
+					FromAccountID: fromAccountID,
+					ToAccountID: toAccountID,
+					Amount: amount,
+				})
+				errs <- err
+				// send error back to the main go routine and check from there
+			}()
+		}
+			// check result
+			for i := 0; i < n; i++{
+				err := <- errs
+				require.NoError(t, err)
+			}
+	
+			// check final updated balance
+			updatedAccount1, err := testQueries.GetAccount(context.Background(), account1.ID)
+			require.NoError(t, err)
+			updatedAccount2, err := testQueries.GetAccount(context.Background(), account2.ID)
+			require.NoError(t, err)
+			
+			// writing logs (after all transactions)
+			fmt.Println(">> after:", updatedAccount1.Balance, updatedAccount2.Balance)
+			require.Equal(t, account1.Balance, updatedAccount1.Balance)
+			require.Equal(t, account2.Balance, updatedAccount2.Balance)
+		}
